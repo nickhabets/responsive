5abd7a02b2541a79d47ac662fe4d8927c1e0020a
Sasa <sasa.simovic931ca@gmail.com>
2017-05-04 15:30:42 +0200

    title

--- src/App.js
+++ src/App.js
@@ -7,12 +7,12 @@ class App extends Component {
     return (
       <div className="App">
         <div className="App-header">
-          <h2>Welcome to Responsive!</h2>
+          <h2>Responsive</h2>
         </div>
         <div className="item-container">
           <h3>Load me in a mobile phone!</h3>
           <div className="item-middle-div">
-            <div className="col-md-4">
+            <div className="col-sm-4">
               <img className="item-img" src="img/hotel.jpg"/>
               <div className="item-label-container">
                 <div className="item-label">Hotels</div>
@@ -20,14 +20,14 @@ class App extends Component {
               <div className="item-label-opacity"/>
             </div>
           </div>
-          <div className="col-md-4">
+          <div className="col-sm-4">
             <img className="item-img" src="img/shop.jpg"/>
             <div className="item-label-container">
               <div className="item-label">Shops</div>
             </div>
             <div className="item-label-opacity"/>
           </div>
-          <div className="col-md-4">
+          <div className="col-sm-4">
             <img className="item-img" src="img/fitness.jpg"/>
             <div className="item-label-container">
               <div className="item-label">Fitness</div>
