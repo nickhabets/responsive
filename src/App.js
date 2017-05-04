688de820117cac38b41a0744f54dc713f7cfdb97
Sasa <sasa.simovic931ca@gmail.com>
2017-05-04 14:52:31 +0200

    hover and click effect

--- src/App.js
+++ src/App.js
@@ -7,12 +7,34 @@ class App extends Component {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to Responsive!</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="item-container">
+          <h3>Load me in a mobile phone!</h3>
+          <div className="item-middle-div">
+            <div className="col-md-4">
+              <img className="item-img" src="img/hotel.jpg"/>
+              <div className="item-label-container">
+                <div className="item-label">Hotels</div>
+              </div>
+              <div className="item-label-opacity"/>
+            </div>
+          </div>
+          <div className="col-md-4">
+            <img className="item-img" src="img/shop.jpg"/>
+            <div className="item-label-container">
+              <div className="item-label">Shops</div>
+            </div>
+            <div className="item-label-opacity"/>
+          </div>
+          <div className="col-md-4">
+            <img className="item-img" src="img/fitness.jpg"/>
+            <div className="item-label-container">
+              <div className="item-label">Fitness</div>
+            </div>
+            <div className="item-label-opacity"/>
+          </div>
+        </div>
       </div>
     );
   }
